b7af3252ec071827c23ab3d8850fc25edb1599b6
Britte <britte.genzlinger@gmail.com>
2024-11-07 10:03:10 +0800

    remove riddler references from test

--- test_riddle_client.py
+++ test_riddle_client.py
@@ -1,18 +1,18 @@
 ######################################
 #   This is a simple test file to ensure the 
-#   RiddlerClient works properly. 
+#   RiddleClient works properly. 
 #
 #   Add in your old tests to be confident 
-#   each of your RiddlerClient methods work
+#   each of your RiddleClient methods work
 ######################################
 
 from riddle_client import RiddleClient
 
-riddler_client = RiddleClient()
+riddle_client = RiddleClient()
 
 print("--- TEST ALL RIDDLES ---")
 
-riddle_list = riddler_client.all_riddles()
+riddle_list = riddle_client.all_riddles()
 
 for riddle in riddle_list:
     print(riddle)
@@ -25,7 +25,7 @@ print("--- TEST GUESS RIDDLES ---")
 
 id = 17
 guess = "to get to the other side"
-guess_riddle_message = riddler_client.guess_riddle(id,guess)
+guess_riddle_message = riddle_client.guess_riddle(id,guess)
 
 
 print(guess_riddle_message)
